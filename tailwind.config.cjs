da9fbab54a37177a95aca6bbe1c97fa87e340043
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-02 17:10:48 +0530

    Add: Custom Theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "hsl(172, 67%, 45%)",
+                neutral: {
+                    50: "hsl(189, 41%, 97%)",
+                    100: "hsl(185, 41%, 84%)",
+                    200: "hsl(184, 14%, 56%)",
+                    300: "hsl(186, 14%, 43%)",
+                    700: "hsl(183, 100%, 15%)",
+                },
+            },
+            fontFamily: {
+                space: ["Space Mono", "monospace"],
+            },
+        },
     },
     plugins: [],
 };
