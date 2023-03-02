a83243b1712ef4332d7d5c519150f3421b533cca
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-02 20:05:43 +0530

    Add: Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { SiteLogo } from "@assets/index";
+
+export default function Header() {
+    return (
+        <header className="flex items-center justify-center">
+            <SiteLogo />
+        </header>
+    );
+}
