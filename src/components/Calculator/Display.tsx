d2b18d51634a64d62ebf0e114ede8788c2f57d7d
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-03 11:47:43 +0530

    Add: Calculator Display Component
    Display Component Includes:
    - Display Container Component
    - Display List Item
    - Display Action Button

---
+++ src/components/Calculator/Display.tsx
@@ -0,0 +1,66 @@
+import {
+    type ReactNode,
+    type DetailedHTMLProps,
+    type ButtonHTMLAttributes,
+} from "react";
+
+import { BiDollar as DollarIcon } from "react-icons/bi";
+
+export default function Display({
+    children,
+    action,
+}: {
+    children: ReactNode;
+    action?: ReactNode;
+}) {
+    return (
+        <div className="grid grid-rows-[1fr_auto] p-10 bg-neutral-700 rounded-2xl">
+            <ul className="flex flex-col gap-5">{children}</ul>
+            {action}
+        </div>
+    );
+}
+
+Display.Item = ({
+    title,
+    subtitle,
+    cost,
+}: {
+    title: string;
+    subtitle: string;
+    cost: string;
+}) => {
+    return (
+        <li className="flex items-center justify-between py-5">
+            <div className="flex flex-col">
+                <span className="text-sm text-white font-semibold capitalize">
+                    {title}
+                </span>
+                <span className="text-sm text-neutral-200">{subtitle}</span>
+            </div>
+            <div className="flex items-center text-primary text-5xl font-bold">
+                <DollarIcon />
+                <span>{cost}</span>
+            </div>
+        </li>
+    );
+};
+
+Display.Button = ({
+    children,
+    className,
+    ...props
+}: DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+>) => {
+    return (
+        <button
+            type="button"
+            {...props}
+            className={`p-2 text-lg font-semibold uppercase rounded-md bg-neutral-100 text-neutral-200 opacity-50 hover:opacity-100 ${className}`}
+        >
+            {children}
+        </button>
+    );
+};
